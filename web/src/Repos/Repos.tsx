aa76d7075f99a6db843c69805a9935f80a2c3dae
Armen Bagramian <armen.bagramian@matador.ai>
2022-06-03 18:18:53 -0400

    feat: display repos with react-table

--- web/src/Repos/Repos.tsx
+++ web/src/Repos/Repos.tsx
@@ -1,18 +1,69 @@
+import { useState } from 'react';
 import { useRepos } from './useRepos';
 import type { Repo } from '../../../api/src/models/Repo';
+import {
+  createTable,
+  useTableInstance,
+  getCoreRowModel,
+} from '@tanstack/react-table';
 import './Repos.css';
+
+const table = createTable().setRowType<Repo>();
+
+const defaultColumns = [
+  table.createGroup({
+    header: 'Repositories',
+    columns: [
+      table.createDataColumn('name', {
+        cell: (info) => info.getValue(),
+      }),
+      table.createDataColumn('description', {
+        cell: (info) => info.getValue(),
+      }),
+      table.createDataColumn('language', {
+        cell: (info) => info.getValue(),
+      }),
+      table.createDataColumn('forks_count', {
+        cell: (info) => info.getValue(),
+      }),
+    ],
+  }),
+];
 
 export function Repos() {
   const { repos, isLoading, error } = useRepos();
+  const [columns] = useState<typeof defaultColumns>(() => [...defaultColumns]);
+  const instance = useTableInstance(table, {
+    data: repos,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <div>
       {isLoading && 'isLoading...'}
-      {repos.map((repo: Repo) => (
-        <div key={repo.id}>
-          {repo.name} {repo.description} {repo.language} {repo.forks_count}
-        </div>
-      ))}
+      <table>
+        <thead>
+          {instance.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : header.renderHeader()}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {instance.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{cell.renderCell()}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
       {error && <div>{error}</div>}
     </div>
   );
