f5574193f0ed53e256b4088578251776af9973c5
Armen Bagramian <armen.bagramian@matador.ai>
2022-06-03 16:08:02 -0400

    feat: implement /repos route

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,27 @@
 import { Router, Request, Response } from 'express';
+import localRepos from '../../data/repos.json';
+import fetch from 'node-fetch';
+import type { Repo } from '../models/Repo';
 
 export const repos = Router();
+
+const fetchRemoteRepos = async (): Promise<any> => {
+  const response = await fetch(
+    ' https://api.github.com/users/silverorange/repos'
+  );
+  const json = await response.json();
+  const correctRepos = json.filter((repo: Repo) => repo.fork === false);
+
+  return correctRepos;
+};
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
+  const correctLocalRepos = localRepos.filter((repo) => repo.fork === false);
+  const correctRemoteRepos = await fetchRemoteRepos();
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.status(200);
+  res.set('Content-Type', 'application/json');
+  res.json([...correctLocalRepos, ...correctRemoteRepos]);
 });
